ab9fb7f5b641216923b730b2e58d5c2bab2d5ab3
DaXuan <daxuan111000@163.com>
2024-01-23 07:29:51 +0800

    create

---
+++ R/zzz.R
@@ -0,0 +1,12 @@
+# zzz.R
+
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("Welcome to DXMeSH!🔍")
+  packageStartupMessage("DXMeSH is an R package designed to streamline and enhance your PubMed MeSH term searches.📚")
+  packageStartupMessage("Say goodbye to the days of tedious search term compilation; with DXMeSH, a comprehensive and accurate search is just a click away.✨")
+  packageStartupMessage("Facilitating efficient biomedical literature retrieval, it leaves more time for your valuable research insights.🔬")
+  packageStartupMessage("For guides and more information, start your journey here: https://github.com/YourUsername/DXMeSH 🚀")
+  packageStartupMessage("Author:")
+  packageStartupMessage("  DaXuan (daxuan111000@163.com📧) ")
+  invisible(NULL)
+}
