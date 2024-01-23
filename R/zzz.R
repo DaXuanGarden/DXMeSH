325ae982a494bad6660a5624e533d241e7c7fede
DaXuan <daxuan111000@163.com>
2024-01-23 08:10:49 +0800

    up

--- R/zzz.R
+++ R/zzz.R
@@ -5,7 +5,7 @@
   packageStartupMessage("DXMeSH is an R package designed to streamline and enhance your PubMed MeSH term searches.📚")
   packageStartupMessage("Say goodbye to the days of tedious search term compilation; with DXMeSH, a comprehensive and accurate search is just a click away.✨")
   packageStartupMessage("Facilitating efficient biomedical literature retrieval, it leaves more time for your valuable research insights.🔬")
-  packageStartupMessage("For guides and more information, start your journey here: https://github.com/YourUsername/DXMeSH 🚀")
+  packageStartupMessage("For guides and more information, start your journey here: https://github.com/DaXuanGarden/DXMeSH 🚀")
   packageStartupMessage("Author:")
   packageStartupMessage("  DaXuan (daxuan111000@163.com📧) ")
   invisible(NULL)
