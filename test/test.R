2c422d65e60ea1de327f8582445d5edef4be166e
DaXuan <daxuan111000@163.com>
2024-02-23 16:25:19 +0800

    up

--- test/test.R
+++ test/test.R
@@ -54,3 +54,27 @@ build_vignettes()
 
 # 查看使用说明书
 browseVignettes("DXMeSH")
+
+name.vec <- c("1,3-Diaminopropane;2-Ketobutyric acid;2-Hydroxybutyric acid;2-Methoxyestrone")
+toSend = list(queryList = name.vec, inputType = "name")
+
+library(httr)
+
+# The MetaboAnalyst API url
+call <- "https://www.xialab.ca/api/mapcompounds"
+
+# Use httr::POST to send the request to the MetaboAnalyst API
+# The response will be saved in query_results
+query_results <- httr::POST(call, body = toSend, encode = "json")
+
+# Check if response is ok (TRUE)
+# 200 is ok! 401 means an error has occured on the user's end.
+query_results$status_code==200
+# Parse the response into a table
+# Will show mapping to "hmdb_id", "kegg_id", "pubchem_id", "chebi_id", "metlin_id", "smiles"
+query_results_text <- content(query_results, "text", encoding = "UTF-8")
+query_results_json <- jsonlite::fromJSON(query_results_text, flatten = TRUE)
+query_results_table <- rbind.data.frame(query_results_json)
+
+write.csv(query_results_table,file = "KEGG_id_used")
+
